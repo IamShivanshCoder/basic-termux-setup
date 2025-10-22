9c93752b89fd0a22b7d1c2f0a504eda25a35d4e5
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 18:00:06 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,11 +1,5 @@
 
-pkg update -y  && pkg upgrade -y
+# pkg update -y  && pkg upgrade -y
 
-wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
-
-cd ~/.termux
-wget 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf'
-mv Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf font.ttf
-termux-reload-settings 
-
+# wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
 
