0e141e2d7a6f31fd7e96112ae1c62d3ee34b0a76
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 09:27:21 +0530

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,12 @@
+termux-setup-storage
+pkg update -y  && pkg upgrade -y
+
+pkg install wget
+wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
+
+cd ~/.termux
+wget 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf'
+mv Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf font.ttf
+termux-reload-settings 
+
+
