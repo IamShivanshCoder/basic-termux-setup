dcc9efdb894af9759169f890af891a4990d13bb0
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 18:44:32 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,7 +3,10 @@
 
 # wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
 
-pkg install neovim git lazygit curl fzf tar nodejs-lts libllvm
+pkg install neovim git lazygit curl fzf tar nodejs-lts libllvm zip
 pkg install tree-sitter python
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
+getnf -i JetBrainsMono
