e19fea820b3d5d0ae980933ddb054f414df4742b
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 18:25:41 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -4,4 +4,4 @@
 # wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
 
 pkg install neovim git lazygit curl fzf tar nodejs-lts libllvm
-npm install tree-sitter-cli
+pkg install libtreesitter
