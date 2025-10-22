4e2a1209a4599e90179fd1bf98f81e1a3ff54d11
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 17:43:53 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,4 +1,4 @@
-termux-setup-storage
+
 pkg update -y  && pkg upgrade -y
 
 wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
