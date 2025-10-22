10dc437583e7d526c89d83a4278c859d9887ef33
IamShivanshCoder <160223600+IamShivanshCoder@users.noreply.github.com>
2025-10-22 18:16:32 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,7 +3,3 @@
 
 # wget https://raw.githubusercontent.com/sabamdarif/termux-desktop/refs/heads/main/other/termux-fastest-repo && chmod +x termux-fastest-repo && ./termux-fastest-repo
 
-pkg install curl ncurses-utils zip
-
-curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-nf/main/install.sh | bash
-
